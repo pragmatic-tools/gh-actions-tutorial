2d20c89e18397621014a795bd28a110d9ea4afbe
Theodore Chan <theodore.chan@dexcom.com>
2022-04-06 12:10:57 -0700

    Github Actions 09 - update test

--- test/test.js
+++ test/test.js
@@ -4,6 +4,6 @@ var app = require('../app.js')
 // TODO: Make this test pass!
 describe('GET /', function() {
     it('displays "Hello Dexcom!"', function(done) {
-        request(app).get('/').expect('Hello World!', done);
+        request(app).get('/').expect('Hello Dexcom!', done);
     })
 })
